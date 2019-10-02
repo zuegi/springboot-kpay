e4c62dbbf7d00645a4c13d5a922669e5912cc8b8
usd2835 <rene.weishaupt@raiffeisen.ch>
2019-10-02 10:12:31 +0200

    zuegi - Refactoring PaymentsIncomingSource

--- src/main/java/ch/wesr/kpay/KpayApplication.java
+++ src/main/java/ch/wesr/kpay/KpayApplication.java
@@ -28,54 +28,9 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class KpayApplication {
 
-    @Component
-    public static class PaymentsIncomingSource implements ApplicationRunner {
-        private final MessageChannel paymentIncoming;
-
-        public PaymentsIncomingSource(KpayBindings bindings) {
-            this.paymentIncoming = bindings.paymentIncoming();
-        }
-
-
-        @Override
-        public void run(ApplicationArguments args) throws Exception {
-            List<String> froms = Arrays.asList("peter", "ueli", "simon", "carsten", "lars", "phong", "rene");
-
-            Runnable runnable = () -> {
-                String from = froms.get(new Random().nextInt(froms.size()));
-                String to = froms.get(new Random().nextInt(froms.size()));
-                String txnId = RandomStringUtils.random(10, true, true);
-                BigDecimal bigDecimal = BigDecimalGenerator.get("10.00", "150.00");
-
-                Payment payment = new Payment(txnId, "id", from, to, bigDecimal, Payment.State.complete, System.currentTimeMillis());
-
-                Message<Payment> message = MessageBuilder
-                        .withPayload(payment)
-                        .setHeader(KafkaHeaders.MESSAGE_KEY, payment.getTxnId().getBytes())
-                        .build();
-                try {
-                    this.paymentIncoming.send(message);
-                    log.info("Sent message: " + message.toString());
-                } catch (Exception e) {
-                    log.error(e.getMessage());
-                }
-            };
-            Executors.newScheduledThreadPool(1).scheduleAtFixedRate(runnable, 1, 1, TimeUnit.SECONDS);
-        }
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(KpayApplication.class, args);
     }
 
 }
 
-class BigDecimalGenerator {
-
-    public static BigDecimal get(String minString, String maxString ) {
-        BigDecimal min = new BigDecimal(minString);
-        BigDecimal max = new BigDecimal(maxString);
-        BigDecimal range = max.subtract(min);
-        return min.add(range.multiply(new BigDecimal(Math.random())));
-    }
-}
---
+++ src/main/java/ch/wesr/kpay/payments/model/PaymentsIncomingSource.java
@@ -0,0 +1,66 @@
+package ch.wesr.kpay.payments.model;
+
+import ch.wesr.kpay.KpayBindings;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class PaymentsIncomingSource implements ApplicationRunner {
+    private final MessageChannel paymentIncoming;
+
+    public PaymentsIncomingSource(KpayBindings bindings) {
+        this.paymentIncoming = bindings.paymentIncoming();
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        List<String> froms = Arrays.asList("peter", "ueli", "simon", "carsten", "lars", "phong", "rene");
+
+        Runnable runnable = () -> {
+            String from = froms.get(new Random().nextInt(froms.size()));
+            String to = froms.get(new Random().nextInt(froms.size()));
+            String txnId = RandomStringUtils.random(10, true, true);
+            BigDecimal bigDecimal = BigDecimalGenerator.get("10.00", "150.00");
+
+            Payment payment = new Payment(txnId, "id", from, to, bigDecimal, Payment.State.complete, System.currentTimeMillis());
+
+            Message<Payment> message = MessageBuilder
+                    .withPayload(payment)
+                    .setHeader(KafkaHeaders.MESSAGE_KEY, payment.getTxnId().getBytes())
+                    .build();
+            try {
+                this.paymentIncoming.send(message);
+                log.info("Sent message: " + message.toString());
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        };
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(runnable, 1, 1, TimeUnit.SECONDS);
+    }
+}
+
+
+class BigDecimalGenerator {
+
+    public static BigDecimal get(String minString, String maxString) {
+        BigDecimal min = new BigDecimal(minString);
+        BigDecimal max = new BigDecimal(maxString);
+        BigDecimal range = max.subtract(min);
+        return min.add(range.multiply(new BigDecimal(Math.random())));
+    }
+}
