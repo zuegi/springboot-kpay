b3b15723491f5be7bd78d8610bf9c2608468d145
usd2835 <rene.weishaupt@raiffeisen.ch>
2019-10-05 08:01:07 +0200

    zuegi - Swagger imported and configured * first RestController listing all accounts

---
+++ src/main/java/ch/wesr/kpay/payments/rest/AccountController.java
@@ -0,0 +1,28 @@
+package ch.wesr.kpay.payments.rest;
+
+import ch.wesr.kpay.payments.model.Payment;
+import ch.wesr.kpay.payments.processors.AccountProcessor;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+public class AccountController {
+
+
+    @Autowired
+    private InteractiveQueryService interactiveQueryService;
+
+
+    @GetMapping("list")
+    public KeyValueIterator<String, Payment> listAccounts(){
+
+        ReadOnlyKeyValueStore<String, Payment> paymentStore = interactiveQueryService.getQueryableStore(AccountProcessor.STORE_NAME, QueryableStoreTypes.<String, Payment>keyValueStore());
+
+        return paymentStore.all();
+    }
+}
